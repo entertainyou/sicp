1929d261a0fe30b7b10b7850c97c12ae771fb7fa
HUANG Wei <weih@opera.com>
2013-08-19 23:56:39 +0800

    Exercise 1.15 WIP

--- ch01.el
+++ ch01.el
@@ -16,7 +16,7 @@
                (+ now 1))))
   (if (< n 3)
       n
-      (f2-iter 2 1 0 2)))
+    (f2-iter 2 1 0 2)))
 
 (equal (mapcar 'f '(1 2 3 4 5 6 7 8 9 10))
        (mapcar 'f2 '(1 2 3 4 5 6 7 8 9 10)))
@@ -35,5 +35,45 @@
 ;; mathematical problem, ignored, :p.
 
 
+;; Ex 1.14
+;; coins: 50,25,10,5,1
+cc(11, 5)
+|
+cc(11, 4), cc(-39, 5)
+|
+cc(11, 3), cc(-14, 4) 0
+|
+cc(11, 2), cc(1, 3) 0
+|
+cc(11, 1), cc(6, 2), cc(1, 2), cc(0, 3)
+|
+cc(11, 0), cc(10, 1), cc(6, 1), cc(1, 2), cc(1, 1), cc(-4, 2), 1
+|
+0, cc(10, 0), cc(9, 1), cc(6, 0), cc(5, 1)....
 
+;; Ex 1.15
+(defun cube (x) (* x x x))
 
+(defun p (x) (- (* 3 x) (* 4 (cube x))))
+
+(defun sine (angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+    (p (sine (/ angle 3.0)))))
+
+(defvar *counter*)
+
+(defadvice sine (before before-counter) 'activate
+  (setq *counter* (+ *counter* 1)))
+
+(defvar *count* 0)
+
+(defmacro counter (func &rest args)
+  `(progn
+     (defadvice ,func (before foobarnew2) 'activate
+       (setq *count* (+ *count* 1)))
+     (setq *count* 0)
+     (,func ,@args)
+     *count*))
+
+(counter sine 1215)
