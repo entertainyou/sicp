375d8e70ae0ffcdfe5f153851732b1a548e7c349
HUANG Wei <weih@opera.com>
2013-08-21 11:39:09 +0800

    Work to Ex 1.22

--- ch01.el
+++ ch01.el
@@ -87,3 +87,91 @@ cc(11, 0), cc(10, 1), cc(6, 1), cc(1, 2), cc(1, 1), cc(-4, 2), 1
 ;; b. space: log3(a) number of steps: log3(a)
 
 
+;; Ex 1.16
+
+(defun fast-exp (b n)
+  ;; a * b ^ n is invariant
+  (defun fast-exp-iter (a b n)
+    (cond
+     ((= n 0) a)
+     ((evenp n) (fast-exp-iter a (* b b) (/ n 2)))
+     (t (fast-exp-iter (* a b) b (- n 1)))))
+  (fast-exp-iter 1 b n))
+
+(fast-exp 2 5)
+
+;; Ex 1.17
+;; lazy to do...
+
+;; Ex 1.18
+
+(defun fast-mul (a b)
+  ;; rem + a * b is invariant
+  (defun fast-mul-iter (rem a b)
+    (cond
+     ((= b 0) rem)
+     ((evenp b) (fast-mul-iter rem (* 2 a) (/ b 2)))
+     (t (fast-mul-iter (+ rem a) a (- b 1)))))
+  (fast-mul-iter 0 a b))
+
+(fast-mul 6 7)
+
+
+;; Ex 1.19
+p' = p^2 + q^2
+q' = q^2 + 2pq
+
+;; Ex 1.20
+
+;; ref: http://wiki.drewhess.com/wiki/SICP_exercise_1.20
+normal order: 18
+applicative order: 4
+
+;; Ex 1.21
+
+(defun smallest-divisor (n)
+  (find-divisor n 2))
+(defun find-divisor (n test-divisor)
+  (cond ((> (* test-divisor test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (t (find-divisor n (+ test-divisor 1)))))
+(defun divides? (a b)
+  (= (% b a) 0))
+
+(mapcar 'smallest-divisor '(199 1999 19999))
+
+;; Ex 1.22
+
+(float-time)
+
+(defun prime? (n)
+  (= n (smallest-divisor n)))
+
+(defun prime-test (n)
+  (start-prime-test n (runtime)))
+
+(defun start-prime-test (n time)
+  (if (prime? n)
+      (- (runtime) time)))
+
+(defun search-for-primes (n count)
+  (defun search-for-primes-iter (n count iter acc)
+    (if (= iter count) (/ acc count)
+      (progn
+        (let ((result (prime-test n)))
+          (if result
+              (search-for-primes-iter (+ n 2) count (+ iter 1) (+ acc result))
+            (search-for-primes-iter (+ n 2) count iter acc))))))
+  (search-for-primes-iter n count 0 0))
+
+;; NEEDED, as no tail recursion optimize for ELisp..
+(setq max-lisp-eval-depth 1000000)
+(setq max-specpdl-size 1000000)
+(search-for-primes 1000001 3)
+(search-for-primes 100001 3)
+(search-for-primes 10001 3)
+(search-for-primes 1001 3)
+
+(mapcar (lambda (n)
+          (search-for-primes n 3)) '(1001 10001 100001))
+
